01cfdda717971277e4b0f6a67ab6f8603ab96ddf
ChillyPhilly <sweet.chilly.philly@gmail.com>
2019-07-04 11:46:15 +1000

    Updates comments/readme

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 This is a project for my Udacity course, for which we were provided the skeleton files and required to create tests to check the functionality of the page.
 
 ## How to Run the application
-1. Application is run by opening index.html in the browser
-2. Feeds will be displayed in the main Selection
-3. Click on whichever article interests you!
-4. To select other feeds, click the "Hamburger" (lol) icon in the top left
-5. Scroll to bottom of page to check that tests all pass
+1. Download the repo
+2. Application is run by opening index.html in the browser
+3. Feeds will be displayed in the main Selection
+4. Click on whichever article interests you!
+5. To select other feeds, click the "Hamburger" (lol) icon in the top left
+6. Scroll to bottom of page to check that tests all pass
 
 ### Dependencies
 1. Jasmine-jQuery - https://github.com/velesin/jasmine-jquery - `npm install jasmine-jquery`
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function() {
     });
 
 
-    /* DONE: Write a test that loops through each feed
+    /* Loops through each feed
      * in the allFeeds object and ensures it has a URL defined
      * and that the URL is not empty.
      */
@@ -41,7 +41,7 @@ $(function() {
     });
 
 
-    /* DONE: Write a test that loops through each feed
+    /* Loops through each feed
      * in the allFeeds object and ensures it has a name defined
      * and that the name is not empty.
      */
@@ -55,24 +55,22 @@ $(function() {
   });
 
 
-  /* DONE: Write a new test suite named "The menu" */
+  /* Test suite for "The menu" */
   describe('The menu', function() {
     const menu = document.querySelector('.slide-menu'),
       body = document.querySelector('body'),
       menuIcon = document.querySelector('.menu-icon-link');
-    /* DONE: Write a test that ensures the menu element is
-     * hidden by default. You'll have to analyze the HTML and
-     * the CSS to determine how we're performing the
-     * hiding/showing of the menu element.
+    /* Ensures the menu element is
+     * hidden by default.
      */
     it('is hidden by default', function() {
       expect(body.classList.contains('menu-hidden')).toBe(true);
     });
 
-    /* DONE: Write a test that ensures the menu changes
-     * visibility when the menu icon is clicked. This test
-     * should have two expectations: does the menu display when
-     * clicked and does it hide when clicked again.
+    /* Ensures the menu changes
+     * visibility when the menu icon is clicked. Makes sure
+     * the menu displays when clicked and hides when clicked again.
+     * Independent of "is hidden by default".
      */
     it('changes visibility when menu icon is clicked', function() {
       const spyEvent = spyOnEvent(menuIcon, 'click');
@@ -91,7 +89,7 @@ $(function() {
     });
   });
 
-  /* DONE: Write a new test suite named "Initial Entries" */
+  /* Test suite for "Initial Entries" */
   describe('Initial Entries', function() {
     const feed = document.querySelector('.feed');
 
@@ -101,11 +99,9 @@ $(function() {
       });
     });
 
-    /* DONE: Write a test that ensures when the loadFeed
+    /* Ensures when the loadFeed
      * function is called and completes its work, there is at least
      * a single .entry element within the .feed container.
-     * Remember, loadFeed() is asynchronous so this test will require
-     * the use of Jasmine's beforeEach and asynchronous done() function.
      */
     it('has at least one element in the feed', function(done) {
       expect(feed.childElementCount).not.toBe(0);
@@ -113,7 +109,7 @@ $(function() {
     });
   });
 
-  /* DONE: Write a new test suite named "New Feed Selection" */
+  /* Test suite for "New Feed Selection" */
   describe('New Feed Selection', function() {
     const feed = document.querySelector('.feed'),
           originalFirstChild = feed.firstElementChild;
@@ -123,9 +119,8 @@ $(function() {
         done();
       });
     });
-  /* DONE: Write a test that ensures when a new feed is loaded
+  /* Ensures when a new feed is loaded
    * by the loadFeed function that the content actually changes.
-   * Remember, loadFeed() is asynchronous.
    */
    it('changes content when a new feed is selected', function() {
      expect(feed.firstElementChild).not.toBe(originalFirstChild);
